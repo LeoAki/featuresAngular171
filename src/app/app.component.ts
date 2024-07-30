1aac2ec83bcfe7bb5a185538d2f7cc701718e54d
Leonardo Aquino <laquino@soaint.com>
2024-07-30 15:31:29 -0500

    feat: uso de los features.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputSignalsComponent } from './signal-inputs/input-signals/input-signals.component';
+import { FnsFechaComponent } from './date-fns/fns-fecha/fns-fecha.component';
+import { YoutubePlayerComponent } from './youtube/youtube-player/youtube-player.component';
+import { MapGoogleComponent } from './google-maps/map-google/map-google.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    InputSignalsComponent, FnsFechaComponent, YoutubePlayerComponent, MapGoogleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular171';
+  title = 'Angular 17.1';
 }
